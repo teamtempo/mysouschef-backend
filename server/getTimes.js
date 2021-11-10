90e1b1d8ef2196e93a35b7eb84a13337d5883d09
jamesbalcombe83 <james.balcombe83@gmail.com>
2021-11-10 11:57:43 +0900

    Fix: added more time strings, and split for commas

--- server/getTimes.js
+++ server/getTimes.js
@@ -1,11 +1,11 @@
 function getTimes(text) {
-    const times = [["hr",3600], ["hours",3600], ["mins",60],["min",60], ["sec",1]];
-    const splitText = text.split(" ")
+    const times = [["hr",3600], ["hour",3600], ["hours",3600], ["min",60],["mins",60], ["minute",60], ["minutes",60], ["sec",1], ["second",1], ["seconds",1]];
+
+    const splitText = text.split(/[ ,]+/)
     let number;
     let unit;
 
     times.forEach((time) => {
-        //console.log(time[0])
         let pos = splitText.indexOf(time[0])
         if (pos > -1) {
             number = (splitText[pos-1] * time[1]);
