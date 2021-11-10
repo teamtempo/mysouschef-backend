d34ad2fa192d5b5c055924aa45cb3f55dddf5918
jamesbalcombe83 <james.balcombe83@gmail.com>
2021-11-10 11:36:46 +0900

    Fix: removed unneeded dependencies

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const getTimes = require('./getTimes');
 const recipeScraper = require("recipe-scraper");
 
-
-
 const PORT = process.env.PORT || 5000;
 const app = express();
 
